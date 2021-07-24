c27956b3c6334fd7226695382813ab8f0e836ffc
ikeda0125 <32475655+ikeda0125@users.noreply.github.com>
2021-07-24 20:46:37 +0900

    Initial Commit

---
+++ calculate-GPS/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  calculate-GPS
+//
+//  Created by 池田佳弘 on 2021/07/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
